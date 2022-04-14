20b2cb1d60ff580e6328620180aa26aa75961d56
ammartica <amisadaymartinez@gmail.com>
2022-04-14 01:19:59 +0000

    added class attributes and removed if cycle

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-
-    if os.getenv("HBNB_TYPE_STORAGE" == "db"):
-        place_amenities = relationship("Place", secondary="place_amenity",
-                                       back_populates="amenities")
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   backref="amenities")
