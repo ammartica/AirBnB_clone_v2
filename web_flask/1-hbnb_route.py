d6f6bf1d3cc12084d126c105f151e4ba534fd7ea
ammartica <amisadaymartinez@gmail.com>
2022-05-18 20:01:04 +0000

    ADD file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web app and displays certain messages"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
